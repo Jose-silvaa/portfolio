c76801a426f7911c5cc25ffbb0fd64ce00e4e3ea
Jose-silvaa <jvitorfacanha@gmail.com>
2025-04-27 23:00:09 -0300

    EA2131

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import HomePageMenu from "@/components/home-page-menu/home-page-menu";
+
 export default function Home() {
   return (
     <main>
+      <HomePageMenu/>
     </main>
   );
 }
--- src/components/home-page-menu/home-page-menu.tsx
+++ src/components/home-page-menu/home-page-menu.tsx
@@ -1,68 +1,18 @@
-import Link from "next/link";
-
 
 export default function HomePageMenu(){
     return (
         <header>
-            <nav className="navbar container mx-auto">
-                {/* Logo ou título */}
+            <div className="navbar">
                 <div className="flex-1">
-                    <Link href="/" className="btn btn-ghost text-xl">
-                        MyApp
-                    </Link>
+                    <a className="text-xl">daisyUI</a>
                 </div>
-
-                {/* Menu para desktop */}
-                <div className="hidden md:flex">
+                <div className="flex-none">
                     <ul className="menu menu-horizontal px-1">
-                        <li>
-                            <Link href="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link href="/about">About</Link>
-                        </li>
-                        <li>
-                            <Link href="/contact">Contact</Link>
-                        </li>
+                        <li><a>Link</a></li>
+                        <li><a>Link</a></li>
                     </ul>
                 </div>
-
-                {/* Menu mobile (dropdown) */}
-                <div className="md:hidden">
-                    <div className="dropdown dropdown-end">
-                        <label tabIndex={0} className="btn btn-ghost btn-circle">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-5 w-5"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={2}
-                                    d="M4 6h16M4 12h16M4 18h16"
-                                />
-                            </svg>
-                        </label>
-                        <ul
-                            tabIndex={0}
-                            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 rounded-box w-52"
-                        >
-                            <li>
-                                <Link href="/">Home</Link>
-                            </li>
-                            <li>
-                                <Link href="/about">About</Link>
-                            </li>
-                            <li>
-                                <Link href="/contact">Contact</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+            </div>
         </header>
     )
 }
