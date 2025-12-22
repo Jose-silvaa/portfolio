e544c5a7c1a8c9b9a56e0f6567f75047b2281b3e
Jose-silvaa <jvitorfacanha@gmail.com>
2025-12-21 22:08:59 -0300

    add new projects

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -10,6 +10,7 @@ export default function Projects(){
             link: "https://civis-conectados-i5zxympap-jose-vitor-s-projects-e0a86c12.vercel.app/",
             image: "/project-civis.png",
             Technologies: ["NextJS", "React", "JavaScript", "Firebase"],
+            repository: "https://github.com/Jose-silvaa/Civis-Conectados",
         },
         {
             title: "Rehabit",
@@ -17,23 +18,36 @@ export default function Projects(){
             link: "https://frontend-rehabit.vercel.app/",
             image: "/project-rehabit.png",
             Technologies: ["NextJS", "React", "JavaScript", "Firebase"],
+            repository: "https://github.com/Jose-silvaa/frontend-rehabit",
         },
+        {
+            title: "Biblioteca",
+            description: "A Library Management System is a software application designed to manage and organize the daily operations of a library efficiently",
+            image: "/project-biblioteca.png",
+            link: "https://biblioteca-frontend-one.vercel.app/",
+            Technologies: ["C#", ".NET", "Postgresql", "Angular"],
+            repository: "https://gitlab.com/Jose02/backend",
+
+        }
     ];
 
     return (
         <section id="projects" className="mb-30 pt-5">
             <h2 className="text-3xl px-10 md:px-20 font-semibold">Projects</h2>
-            <div className="flex flex-col sm:flex-row md:justify-between items-center">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 items-center">
             {projects.map((project, index) => (
-                <Link href={project.link} key={index}>
-                    <section className="md:py-10 md:px-20 px-10 py-10">
-                        <div className="text-justify">
+                <section className="md:py-10 md:px-20 px-10 py-10" key={index}>
+                    <div className="text-justify">
+                        <Link href={project.link} key={index}>
                             <Image src={project.image} alt="foto" width="700" height="100" className="rounded-md object-cover mb-10" />
-                            <h3 className="font-bold text-2xl mb-2">{project.title}</h3>
-                            <span>{project.description}</span><br></br>
-                        </div>
-                    </section>
-                </Link>
+                        </Link>
+                        <h3 className="font-bold text-2xl mb-5">{project.title}</h3>
+                        <span>{project.description}</span>
+                        <br></br>
+                        <h3 className="font-semibold mt-3 mb-3">Repository: <Link href={project.repository} key={index}> {project.repository}</Link></h3>
+                        <p>Technologies : {project.Technologies.join(', ')}</p>
+                    </div>
+                </section>
             ))}
 
             </div>
